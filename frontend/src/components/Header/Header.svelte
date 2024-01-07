b28eaa96033e4c75495e4f8dfe578ae4195b0892
chinurum-omenuko <omen0003@algonquinlive.com>
2024-01-06 21:46:57 -0500

    adds path names to header

--- frontend/src/components/Header/Header.svelte
+++ frontend/src/components/Header/Header.svelte
@@ -1,11 +1,16 @@
-<script>
-
+<script lang="ts">
+    import { page } from "$app/stores";
 </script>
 
 <div class="navbar bg-black nav">
-    <div class="flex-1 d-flex flex-row justify-center items-center">
+    <div class="flex-1 d-flex flex-row justify-between items-center">
       <!-- svelte-ignore a11y-missing-attribute -->
       <a class="btn btn-ghost text-xl">Infeu</a>
+      <div class="page"><h2>{
+        $page.url.pathname === "/" ? "Map" :
+        $page.url.pathname === "/news" ? "News" :
+        $page.url.pathname === "/report" ? "Report" : ""
+    }</h2></div>
     </div>
     
 </div>
