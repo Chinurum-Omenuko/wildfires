91e47391d4ecfd3bcda4e39e98c560d9582d6bc1
chinurum-omenuko <omen0003@algonquinlive.com>
2024-01-08 03:05:46 -0500

    adds a comment

--- frontend/src/routes/report/view/+page.server.ts
+++ frontend/src/routes/report/view/+page.server.ts
@@ -3,4 +3,6 @@ export const load = async () =>{
     const response = await fetch('https://infeu.onrender.com/report/')
     const report =  response.json()
     return {report}
-}
+} 
+
+// comment
