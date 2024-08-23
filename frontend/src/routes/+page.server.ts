7e099e2c123ea87e282ee42c5d98e58c62a47912
Chinurum-Omenuko <omen0003@algonquinlive.com>
2024-08-23 03:07:49 -0400

    change to maps

--- frontend/src/routes/+page.server.ts
+++ frontend/src/routes/+page.server.ts
@@ -3,7 +3,7 @@ export const ssr = false
 
 /** @type {import('./$types').PageServerLoad} */
 export const load = async () => {
-    const res = await fetch('https://infeu.onrender.com/map/')
+    const res = await fetch('https://infeu.onrender.com/map')
     const fires = await res.json()
     console.log(fires)
     return { fires }
